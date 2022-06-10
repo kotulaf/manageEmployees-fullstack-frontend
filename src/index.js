48230a08ed2ee3ae322dc4d2d161b7e3bbdb8ca0
Filip Kotula <fkotula1121@gmail.com>
2022-06-10 13:43:42 +0200

    init, files uploaded and routes configuring

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // we are passing the App method into the render method of react dom and it will render inside the dom element
 root.render(
-  <React.StrictMode>
+  <React.StrictMode>  
     <App />
-  </React.StrictMode>
+  </React.StrictMode> 
 );
 
 // If you want to start measuring performance in your app, pass a function
